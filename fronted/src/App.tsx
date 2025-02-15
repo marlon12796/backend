dfc7341768b26f803afeb73c80a76d3d0a8f19ab
marlon ureta <fabrisio021@gmail.com>
2025-02-14 23:53:41 -0500

    added page callback

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import { Toaster } from "react-hot-toast";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 function App() {
   return (
     <>
@@ -9,6 +10,8 @@ function App() {
           path='/sso-callback'
           element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
         />
+
+        <Route path='/auth-callback' element={<AuthCallbackPage />} />
       </Routes>
       <Toaster />
     </>
